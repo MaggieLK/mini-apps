c2e3f6434aa2474941b9ff9a0f70d9978647b2b8
Maggie Kasunic <maggie.kasunic@gmail.com>
2020-10-17 14:57:41 -0500

    Disable additional pieces after win

--- challenge_4/client/src/components/Board.jsx
+++ challenge_4/client/src/components/Board.jsx
@@ -1,7 +1,7 @@
 class Board extends React.Component {
   constructor(props) {
     super(props);
-    this.state = { counter: 0, color: "yellowSpace", win: '', red: [], yellow: [] };
+    this.state = { counter: 0, color: "yellowSpace", win: '', red: [], yellow: [], disabled: false};
     this.handleClick = this.handleClick.bind(this);
     this.winTest = this.winTest.bind(this);
   }
@@ -10,16 +10,15 @@ class Board extends React.Component {
     var column = document.getElementById(`col${e.target.dataset['x']}`)
     let color = this.state.color
     let freeSpace = null;
+    var allPieces = [];
     for(var i = 1; i<=6; i++) {
       if (column.getElementsByClassName(`space ${i}`).length > 0){
         freeSpace = column.getElementsByClassName(`space ${i}`)[0];
         break;
       }
     }
-    if(freeSpace !== null) {
+    if(freeSpace !== null && this.state.disabled == false) {
       var spaceLoc = parseInt(`${freeSpace.dataset['x']}${freeSpace.dataset['y']}`);
-      var allPieces = [];
-      console.log(spaceLoc)
       this.setState({
         counter: this.state.counter + 1
       });
@@ -43,86 +42,18 @@ class Board extends React.Component {
         })
       }
     }
-    if (this.state.counter >= 41) {
-      this.setState({
-        win: "It's a tie!"
-      })
-    }
     allPieces.sort((a,b) => { return a-b;})
-    this.winTest(allPieces);
-
-
+    winTest(allPieces, color);
   }
 
-  winTest(allPieces) {
-    //horizontal
-    var horWin = 1;
-    for (var i = 0; i < allPieces.length; i++) {
-      if (allPieces.indexOf(allPieces[i] + 10) !== -1) {
-        horWin++
-        i = allPieces.indexOf(allPieces[i] + 10) -1;
-      } else {
-        horWin = 1;
-      }
-      if(horWin === 4) {
-        this.setState({
-          win: "Win"
-        })
-      }
-    }
-    //vertical
-    var vertWin = 1;
-    for (var i = 0; i < allPieces.length; i++) {
-      if (allPieces.indexOf(allPieces[i] + 1) !== -1) {
-        vertWin++
-      } else {
-        vertWin = 1;
-      }
-      if(vertWin === 4) {
-        this.setState({
-          win: "Win"
-        })
-      }
-    }
-    //diagonalUp
-    var diagUpWin = 1;
-    for (var i = 0; i < allPieces.length; i++) {
-      if (allPieces.indexOf(allPieces[i] + 11) !== -1) {
-        diagUpWin++
-        i = allPieces.indexOf(allPieces[i] + 11) -1;
-      } else {
-        diagUpWin = 1;
-      }
-      if(diagUpWin === 4) {
-        this.setState({
-          win: "Win"
-        })
-      }
-    }
-    //diagonalDown
-    var diagDownWin = 1;
-    for (var i = 0; i < allPieces.length; i++) {
-      if (allPieces.indexOf(allPieces[i] + 9) !== -1) {
-        diagDownWin++
-        i = allPieces.indexOf(allPieces[i] + 9) -1;
-        console.log('wincount', diagDownWin)
-      } else {
-        diagDownWin = 1;
-      }
-      if(diagDownWin === 4) {
-        this.setState({
-          win: "Win"
-        })
-      }
-    }
-  }
+
 
   render () {
     return (
       <div>
         <h1 id="title">Connect 4</h1>
         <div id="board">
-          <div id="col1" onClick={this.handleClick}>
+          <div id="col1" onClick={this.handleClick} disabled = {this.state.disabled}>
             <div data-x="1" data-y="6" className="space 6"></div>
             <div data-x="1" data-y="5" className="space 5"></div>
             <div data-x="1" data-y="4" className="space 4"></div>
@@ -185,4 +116,83 @@ class Board extends React.Component {
   }
 }
 
-export {Board}
+winTest(allPieces, color) {
+  if (color === "yellowSpace") {
+    color = "Yellow"
+  } else {
+    color = "Red"
+  }
+  //horizontal
+  var horWin = 1;
+  for (var i = 0; i < allPieces.length; i++) {
+    if (allPieces.indexOf(allPieces[i] + 10) !== -1) {
+      horWin++
+      i = allPieces.indexOf(allPieces[i] + 10) -1;
+    } else {
+      horWin = 1;
+    }
+    if(horWin === 4) {
+      Board.setState({
+        win: `${color} Wins!`,
+        disabled: true
+      })
+    }
+  }
+  //vertical
+  var vertWin = 1;
+  for (var i = 0; i < allPieces.length; i++) {
+    if (allPieces.indexOf(allPieces[i] + 1) !== -1) {
+      vertWin++
+    } else {
+      vertWin = 1;
+    }
+    if(vertWin === 4) {
+      Board.setState({
+        win: `${color} Wins!`,
+        disabled: true
+      })
+    }
+  }
+  //diagonalUp
+  var diagUpWin = 1;
+  for (var i = 0; i < allPieces.length; i++) {
+    if (allPieces.indexOf(allPieces[i] + 11) !== -1) {
+      diagUpWin++
+      i = allPieces.indexOf(allPieces[i] + 11) -1;
+    } else {
+      diagUpWin = 1;
+    }
+    if(diagUpWin === 4) {
+      Board.setState({
+        win: `${color} Wins!`,
+        disabled: true
+      })
+    }
+  }
+  //diagonalDown
+  var diagDownWin = 1;
+  for (var i = 0; i < allPieces.length; i++) {
+    if (allPieces.indexOf(allPieces[i] + 9) !== -1) {
+      diagDownWin++
+      i = allPieces.indexOf(allPieces[i] + 9) -1;
+    } else {
+      diagDownWin = 1;
+    }
+    if(diagDownWin === 4) {
+      Board.setState({
+        win: `${color} Wins!`,
+        disabled: true
+      })
+    }
+  }
+  //tie
+  if (Board.state.counter >= 41) {
+    Board.setState({
+      win: "It's a tie!",
+      disabled: true
+    })
+  }
+}
+
+export const Board = Board;
+export const winTest = winTest;
