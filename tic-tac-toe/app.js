2dd5efe95979f608cd9646fc731f198c59484668
Maggie Kasunic <maggie.kasunic@gmail.com>
2021-01-20 18:04:35 -0600

    make rotation feature optional

--- tic-tac-toe/app.js
+++ tic-tac-toe/app.js
@@ -5,7 +5,8 @@ const xPlayer = document.getElementById('xPlayer')
 const oWinCount = document.getElementById('oWins')
 const xWinCount = document.getElementById('xWins')
 const button = document.querySelector('button');
-const winBox = document.getElementById('winBox')
+const winBox = document.getElementById('winBox');
+const rotator = document.getElementById('rotator');
 
 var gameState = {};
 gameState.xPieces = [];
@@ -14,12 +15,12 @@ gameState.newPiece = 'X'
 gameState.oWins = 0;
 gameState.xWins = 0;
 gameState.rotate = 0;
-gameState.xPlayer = window.prompt("Who is player X?")
-gameState.oPlayer = window.prompt("Who is player O?")
+// gameState.xPlayer = window.prompt("Who is player X?")
+// gameState.oPlayer = window.prompt("Who is player O?")
 
 var winCombos = [['one', 'two', 'three'], ['four', 'five', 'six'], ['seven', 'eight', 'nine'], ['one', 'four', 'seven'], ['two', 'five', 'eight'], ['three', 'six', 'nine'], ['one', 'five', 'nine'], ['three', 'five', 'seven']];
-xPlayer.innerHTML = ` X: ${gameState.xPlayer} `
-oPlayer.innerHTML = ` O: ${gameState.oPlayer} `
+// xPlayer.innerHTML = ` X: ${gameState.xPlayer} `
+// oPlayer.innerHTML = ` O: ${gameState.oPlayer} `
 
 const newGame = () => {
   for (var i = 0; i < allPieces.length; i++) {
@@ -40,23 +41,23 @@ piece.addEventListener('click', event => {
       gameState.oPieces.push(event.target.id)
       gameState.newPiece = 'X'
     }
-    spinner();
+    if (rotator.checked) spinner()
   }
   for (var i = 0; i < winCombos.length; i++){
     if (winCombos[i].every((piece) => gameState.xPieces.indexOf(piece) > -1)) {
       gameState.newPiece = 'O';
       gameState.xWins += 1;
-      winBox.innerHTML = `X: ${gameState.xPlayer} wins!`;
+      winBox.innerHTML = `X wins!`;
       xWinCount.innerHTML = gameState.xWins;
     }
     if (winCombos[i].every(piece => gameState.oPieces.indexOf(piece) > -1)) {
       gameState.newPiece = 'X'
       gameState.oWins += 1;
-      winBox.innerHTML = `O: ${gameState.oPlayer} wins!`;
+      winBox.innerHTML = `O wins!`;
       oWinCount.innerHTML = gameState.oWins;
     }
   }
-  if (gameState.rotate >= 810) {
+  if (gameState.xPieces.length + gameState.oPieces.length == 9 && winBox.innerHTML === '') {
     winBox.innerHTML = `It's a tie!`;
   }
 });
